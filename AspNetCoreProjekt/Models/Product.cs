9c55d253f84fdc961620805cb7cf7df7ca65e89a
Perly Gustafsson <contact@daperley.com>
2017-09-07 11:59:10 +0200

    Validation added to Product

--- AspNetCoreProjekt/Models/Product.cs
+++ AspNetCoreProjekt/Models/Product.cs
@@ -1,9 +1,13 @@
-﻿namespace AspNetCoreProjekt.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreProjekt.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Range(5, 1000)]
         public decimal Price { get; set; }
     }
 }
